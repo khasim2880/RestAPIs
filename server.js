9af8719a3d4280880035ece6467fb31a84825c5d
Khasim-AlRayan <khasim2880@gmail.com>
2019-05-21 17:02:30 +0530

    products

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 
 /* CORS */
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000, https://my-restapis.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.setHeader("Content-Type", "application/json");
     next();

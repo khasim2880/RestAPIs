d71c334f6c0a3c5f041cb9e6aa4f17e0790b58f8
Khasim-AlRayan <khasim2880@gmail.com>
2019-05-21 16:58:47 +0530

    products

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ let bodyParser  = require('body-parser');
 let mongoose    = require('mongoose');
 global.config = require('./config/config');
 
-mongoose.connect("mongodb+srv://khasim:12345@cluster0-mglvd.mongodb.net/backend?retryWrites=true", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://khasim:12345@cluster0-mglvd.mongodb.net/backend?retryWrites=true&ssl=true", { useNewUrlParser: true });
 app.use(bodyParser.json());
 
 /* CORS */
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000, https://my-restapis.herokuapp.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.setHeader("Content-Type", "application/json");
     next();
